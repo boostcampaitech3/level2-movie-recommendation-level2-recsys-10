fab5c74d16e87856d7ce2fb3b4fe052f7f88abca
styoo <yst3147@naver.com>
2022-03-31 06:42:09 +0000

    styoo | models.py | feat : Bert4Rec Model Add

--- input/code/models.py
+++ input/code/models.py
@@ -237,3 +237,82 @@ class S3RecModel(nn.Module):
             module.weight.data.fill_(1.0)
         if isinstance(module, nn.Linear) and module.bias is not None:
             module.bias.data.zero_()
+
+class BERT4RecModel(nn.Module):
+    def __init__(self, args):
+        super(BERT4RecModel, self).__init__()
+        self.item_embeddings = nn.Embedding(
+            args.item_size, args.hidden_size, padding_idx=0
+        )
+        self.position_embeddings = nn.Embedding(args.max_seq_length, args.hidden_size)
+        self.item_encoder = Encoder(args)
+        self.LayerNorm = LayerNorm(args.hidden_size, eps=1e-12)
+        self.dropout = nn.Dropout(args.hidden_dropout_prob)
+        self.out = nn.Linear(args.hidden_size, args.item_size - 1)
+        self.args = args
+
+        #self.criterion = nn.BCELoss(reduction="none")
+        self.apply(self.init_weights)
+
+    def add_position_embedding(self, sequence):
+
+        seq_length = sequence.size(1)
+        position_ids = torch.arange(
+            seq_length, dtype=torch.long, device=sequence.device
+        )
+        position_ids = position_ids.unsqueeze(0).expand_as(sequence)
+        item_embeddings = self.item_embeddings(sequence)
+        position_embeddings = self.position_embeddings(position_ids)
+        sequence_emb = item_embeddings + position_embeddings
+        sequence_emb = self.LayerNorm(sequence_emb)
+        sequence_emb = self.dropout(sequence_emb)
+
+        return sequence_emb
+
+    # Fine tune
+    # same as SASRec
+    def forward(self, input_ids):
+
+        attention_mask = (input_ids > 0).long()
+        extended_attention_mask = attention_mask.unsqueeze(1).repeat(1, input_ids.shape[1], 1).unsqueeze(1).long()
+        # attention_mask = (input_ids > 0).long()
+        # extended_attention_mask = attention_mask.unsqueeze(1).unsqueeze(
+        #     2
+        # )  # torch.int64
+        # max_len = attention_mask.size(-1)
+        # attn_shape = (1, max_len, max_len)
+        # subsequent_mask = torch.triu(torch.ones(attn_shape), diagonal=1)  # torch.uint8
+        # subsequent_mask = (subsequent_mask == 0).unsqueeze(1)
+        # subsequent_mask = subsequent_mask.long()
+
+        if self.args.cuda_condition:
+            extended_attention_mask = extended_attention_mask.cuda()
+
+        # extended_attention_mask = extended_attention_mask * subsequent_mask
+        extended_attention_mask = extended_attention_mask.to(
+            dtype=next(self.parameters()).dtype
+        )  # fp16 compatibility
+        extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
+
+        sequence_emb = self.add_position_embedding(input_ids)
+
+        item_encoded_layers = self.item_encoder(
+            sequence_emb, extended_attention_mask, output_all_encoded_layers=True
+        )
+
+        sequence_output = item_encoded_layers[-1]
+        sequence_output = self.out(sequence_output)
+        
+        return sequence_output
+
+    def init_weights(self, module):
+        """Initialize the weights."""
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            # Slightly different from the TF version which uses truncated_normal for initialization
+            # cf https://github.com/pytorch/pytorch/pull/5617
+            module.weight.data.normal_(mean=0.0, std=self.args.initializer_range)
+        elif isinstance(module, LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        if isinstance(module, nn.Linear) and module.bias is not None:
+            module.bias.data.zero_()
--- input/code/trainers.py
+++ input/code/trainers.py
@@ -302,3 +302,103 @@ class FinetuneTrainer(Trainer):
                 return pred_list
             else:
                 return self.get_full_sort_score(epoch, answer_list, pred_list)
+
+class BERT4RecTrainer(Trainer):
+    def __init__(
+        self,
+        model,
+        train_dataloader,
+        eval_dataloader,
+        test_dataloader,
+        submission_dataloader,
+        args,
+    ):
+        super(FinetuneTrainer, self).__init__(
+            model,
+            train_dataloader,
+            eval_dataloader,
+            test_dataloader,
+            submission_dataloader,
+            args,
+        )
+
+    def iteration(self, epoch, dataloader, mode="train"):
+
+        # Setting the tqdm progress bar
+
+        rec_data_iter = tqdm.tqdm(
+            enumerate(dataloader),
+            desc="Recommendation EP_%s:%d" % (mode, epoch),
+            total=len(dataloader),
+            bar_format="{l_bar}{r_bar}",
+        )
+        if mode == "train":
+            self.model.train()
+            rec_avg_loss = 0.0
+            rec_cur_loss = 0.0
+
+            for i, batch in rec_data_iter:
+                # 0. batch_data will be sent into the device(GPU or CPU)
+                batch = tuple(t.to(self.device) for t in batch)
+                _, input_ids, target_pos, target_neg, _ = batch
+                # Binary cross_entropy
+                sequence_output = self.model.finetune(input_ids)
+                loss = self.cross_entropy(sequence_output, target_pos, target_neg)
+                self.optim.zero_grad()
+                loss.backward()
+                self.optim.step()
+
+                rec_avg_loss += loss.item()
+                rec_cur_loss = loss.item()
+
+            post_fix = {
+                "epoch": epoch,
+                "rec_avg_loss": "{:.4f}".format(rec_avg_loss / len(rec_data_iter)),
+                "rec_cur_loss": "{:.4f}".format(rec_cur_loss),
+            }
+
+            if (epoch + 1) % self.args.log_freq == 0:
+                print(str(post_fix))
+
+        else:
+            self.model.eval()
+
+            pred_list = None
+            answer_list = None
+            for i, batch in rec_data_iter:
+
+                batch = tuple(t.to(self.device) for t in batch)
+                user_ids, input_ids, _, target_neg, answers = batch
+                recommend_output = self.model.finetune(input_ids)
+
+                recommend_output = recommend_output[:, -1, :]
+
+                rating_pred = self.predict_full(recommend_output)
+
+                rating_pred = rating_pred.cpu().data.numpy().copy()
+                batch_user_index = user_ids.cpu().numpy()
+                rating_pred[self.args.train_matrix[batch_user_index].toarray() > 0] = 0
+
+                ind = np.argpartition(rating_pred, -10)[:, -10:]
+
+                arr_ind = rating_pred[np.arange(len(rating_pred))[:, None], ind]
+
+                arr_ind_argsort = np.argsort(arr_ind)[np.arange(len(rating_pred)), ::-1]
+
+                batch_pred_list = ind[
+                    np.arange(len(rating_pred))[:, None], arr_ind_argsort
+                ]
+
+                if i == 0:
+                    pred_list = batch_pred_list
+                    answer_list = answers.cpu().data.numpy()
+                else:
+                    pred_list = np.append(pred_list, batch_pred_list, axis=0)
+                    answer_list = np.append(
+                        answer_list, answers.cpu().data.numpy(), axis=0
+                    )
+
+            if mode == "submission":
+                return pred_list
+            else:
+                return self.get_full_sort_score(epoch, answer_list, pred_list)
