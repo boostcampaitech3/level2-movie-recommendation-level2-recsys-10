316fb9891e8bf9d75fffcc7eccd12ead485319b1
minchoul2 <sny2128@gmail.com>
2022-04-01 06:56:45 +0000

    Mincheol | utils.py | feat. Add neg_sample_popular

--- input/code/utils.py
+++ input/code/utils.py
@@ -34,6 +34,29 @@ def neg_sample(item_set, item_size):
     return item
 
 
+
+def neg_sample_popular(popular_item_list, item_set, num_neg = 50, neg_sample_by_pos_sample = False,popular_rate = 0.3):
+    """
+    Args:
+        popular_item_list (list): 평가된 횟수 순으로 정렬된 list
+        item_set (set): 유저의 item set
+        num_neg (int): 뽑을 neg_sample 개수 
+        neg_sample_by_pos_sample (boolean): Positive와 동일한 수의 negtive sampling을 할지 여부
+        popular_rate (float): 상위 몇 %의 item을 중에 선택할 것인가 결정하는 float
+    """
+    popular_rate = int(len(popular_item_list)*popular_rate) 
+    popular_item_list = popular_item_list[:popular_rate]
+    popular_item_list = list(set(popular_item_list).difference(item_set))
+    
+    if neg_sample_by_pos_sample:
+        num_neg = len(item_set)
+
+    neg_items = np.random.choice(popular_item_list, num_neg, replace=False)
+
+    return neg_items
+
+    pass
+
 class EarlyStopping:
     """Early stops the training if validation loss doesn't improve after a given patience."""
 
