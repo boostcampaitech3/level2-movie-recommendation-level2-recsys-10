4c2aa923b243b78c7ea56a211cbc8e6011529204
Gyeongtae Im <igt0530@gmail.com>
2022-04-07 06:15:49 +0000

    RecVAE | utils.py | feat: Add increment_path

--- input/vae/utils.py
+++ input/vae/utils.py
@@ -1,9 +1,12 @@
 from scipy import sparse
 import pandas as pd
 import os
+import re
 import bottleneck as bn
 import random, os, torch
 import numpy as np
+from pathlib import Path
+import glob
 
 def set_seed(seed):
     random.seed(seed)
@@ -21,6 +24,22 @@ def check_path(path):
         os.makedirs(path)
         print(f"{path} created")
 
+def increment_path(path, exist_ok=False):
+    """ Automatically increment path, i.e. runs/exp --> runs/exp0, runs/exp1 etc.
+    Args:
+        path (str or pathlib.Path): f"{model_dir}/{args.name}".
+        exist_ok (bool): whether increment path (increment if False).
+    """
+    path = Path(path)
+    if (path.exists() and exist_ok) or (not path.exists()):
+        return str(path)
+    else:
+        dirs = glob.glob(f"{path}*")
+        matches = [re.search(rf"%s(\d+)" % path.stem, d) for d in dirs]
+        i = [int(m.groups()[0]) for m in matches if m]
+        n = max(i) + 1 if i else 2
+        return f"{path}{n}"
+
 def get_lr(optimizer):
     for param_group in optimizer.param_groups:
         return param_group['lr']
@@ -33,8 +52,7 @@ def Recall_at_k_batch(X_pred, label_data, k=100):
     X_pred_binary[np.arange(batch_users)[:, np.newaxis], idx[:, :k]] = True
 
     X_true_binary = (label_data > 0)#.toarray()
-    tmp = (np.logical_and(X_true_binary, X_pred_binary).sum(axis=1)).astype(
-        np.float32)
+    tmp = (np.logical_and(X_true_binary, X_pred_binary).sum(axis=1)).astype(np.float32)
     recall = tmp / np.minimum(k, X_true_binary.sum(axis=1))
     return recall
 
