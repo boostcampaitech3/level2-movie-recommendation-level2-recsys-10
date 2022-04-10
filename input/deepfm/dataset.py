e40f79ba637800e5cb2df0e95d8ac2a9b68401e0
minchoul2 <sny2128@gmail.com>
2022-04-10 11:54:00 +0000

    Mincheol | dataset | add : Add train mode(seq, static)

--- input/deepfm/dataset.py
+++ input/deepfm/dataset.py
@@ -12,7 +12,7 @@ from utils import neg_sample
 
 
 class DeepFMDataset(Dataset):
-    def __init__(self, rating, year, genre, valid_size=10, mode = 'train'):
+    def __init__(self, rating, year, genre, valid_size=10, mode = 'static'):
         self.mode = mode
         self.valid_size = valid_size
 
@@ -38,16 +38,22 @@ class DeepFMDataset(Dataset):
         
     def __getitem__(self,user_id):
         if self.pos_or_neg() > 0.5: # positive
-            pos_set = self.rating_df[user_id][:-self.valid_size]
+            if self.mode == 'seq':
+                pos_set = self.rating_df[user_id][-self.valid_size:]
+            elif self.mode == 'static': # static
+                if self.valid_size >= len(self.rating_df[user_id]):
+                    pos_set = self.rating_df[user_id][:-10]
+                else : pos_set = self.rating_df[user_id][:-self.valid_size]  
             self.item_selected = np.random.choice(pos_set)
             self.target = np.array(1)
 
         else: # negative
-            pos_set = self.rating_df[user_id][:-self.valid_size]
+            pos_set = self.rating_df[user_id]
             self.item_selected = np.random.randint(1, self.n_item - 1)
             while self.item_selected in pos_set:
                 self.item_selected = np.random.randint(1, self.n_item - 1)
             self.target = np.array(0)
+            
         # self.item_selected = self.item_selected
         genre_selected = self.genres_dict[self.item_selected]
         year_selected = self.year_dict[self.item_selected]
