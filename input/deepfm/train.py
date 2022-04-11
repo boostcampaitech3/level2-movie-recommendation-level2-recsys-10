5e66a13aa2c3faf09d3f9f0eef9e6519e7a949c2
minchoul2 <sny2128@gmail.com>
2022-04-11 05:01:10 +0000

    Mincheol | train | refactor : Chang Dimension Bigger

--- input/deepfm/train.py
+++ input/deepfm/train.py
@@ -6,7 +6,7 @@ import pandas as pd
 import torch.nn as nn
 from torch.utils.data import DataLoader
 import torch.optim as optim
-from torch.optim.lr_scheduler import StepLR
+from torch.optim.lr_scheduler import StepLR,ReduceLROnPlateau,CosineAnnealingLR
 from sklearn.preprocessing import MultiLabelBinarizer, LabelEncoder
 
 from utils import precision, recall, set_seed, f1_score
@@ -27,12 +27,13 @@ def main():
     parser.add_argument("--output_dir", default="./output/", type=str)
     parser.add_argument('--model', type=str, default='deepfm', help='Model Name (deepfm)')
     parser.add_argument('--valid_size', type=int, default=10, help='valid size per user')
-    parser.add_argument('--mlp_dims', nargs='+', type=int, default=[30,20,10], help = 'Multi-Layer-Perceptron dimensions list')
-    parser.add_argument('--embedding_dim', type=int, default=10, help='embedding_dim for input tensor')
-    parser.add_argument('--drop_rate', type=float, default=0.1, help='Drop rate')
+    parser.add_argument('--mlp_dims', nargs='+', type=int, default=[200, 200, 200], help = 'Multi-Layer-Perceptron dimensions list')
+    parser.add_argument('--embedding_dim', type=int, default=400, help='embedding_dim for input tensor')
+    parser.add_argument('--drop_rate', type=float, default=0.6, help='Drop rate')
     parser.add_argument('--lr', type=float, default=5e-3, help='learning rate')
     parser.add_argument("--lr_decay_step", type=int, default=100, help="default: 200") 
-    parser.add_argument('--epoch', type=int, default=10000, help='num of epochs')
+    parser.add_argument("--gamma", type=float, default=0.1, help="default: 0.1") 
+    parser.add_argument('--epoch', type=int, default=5000, help='num of epochs')
     
     parser.add_argument("--wandb_name", type=str, default='-', help=" ")
     parser.add_argument("--save_results", action="store_true")
@@ -75,8 +76,6 @@ def main():
             year['item']  = year['item'].map(lambda x : items_dict[x])
             genres['item']  = genres['item'].map(lambda x : items_dict[x])
 
-        
-
         le = LabelEncoder()
         year['year'] = le.fit_transform(year['year'])
         
@@ -106,14 +105,15 @@ def main():
         # config setting
         input_dims = [n_user, n_item, n_year]
         embedding_dim = args.embedding_dim
-
-        model = DeepFM(input_dims, embedding_dim, mlp_dims=[30, 20, 10]).to(device)
+        mlp_dims = args.mlp_dims
+        model = DeepFM(input_dims, embedding_dim, mlp_dims=mlp_dims).to(device)
         bce_loss = nn.BCELoss() # Binary Cross Entropy loss
 
         optimizer = optim.Adam(model.parameters(), lr=args.lr)
-        scheduler = StepLR(optimizer, args.lr_decay_step, gamma=0.1)
-
-        losses = [1]
+        # scheduler = StepLR(optimizer, args.lr_decay_step, gamma=args.gamma)
+        # scheduler = ReduceLROnPlateau(optimizer, patience=100,mode='min')
+        scheduler = CosineAnnealingLR(optimizer, T_max=100, eta_min=0)
+        losses = [100]
         
         print("Training Strat")
         for e in tqdm(range(args.epoch)) :
@@ -129,11 +129,11 @@ def main():
                 loss.backward()
                 optimizer.step()
 
-            scheduler.step()
+            scheduler.step(loss)
             losses.append(loss)
             training_time = time()-t1
-            print(f"[EPOCH: {e :3}/{args.epoch}] [Train] time: {training_time:4.2}s | Loss: {loss:4.4}")
-            
+            print(f"[EPOCH: {e :3}/{args.epoch}] [Train] time: {training_time:4.2}s | Loss: {loss:4.4}", end=' | ')
+            print("[Learning Rate]: {}".format(scheduler.optimizer.state_dict()['param_groups'][0]['lr']))
             # 모델 저장
             if loss == min(losses):
                 save_path = os.path.join(args.output_dir,f'DeepFM_{args.v}.pt')
@@ -164,6 +164,8 @@ def main():
                 print("F1-score : {:.2f}%".format(f1*100), end = '\t')
                 print("Recall : {:.2f}%".format(recall*100), end = '\t')
                 print("Precision : {:.2f}%".format(precision*100))
+
+                
 if __name__ == '__main__':
     main()
 
